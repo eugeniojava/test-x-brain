5b59165285650747a7a124e29f4ac8ca9d2a86d3
Eugênio Moreira <eugeniojava@outlook.com>
2021-03-25 19:01:08 -0200

    initial setup

---
+++ src/main/java/com/eugeniojava/testxbrain/TestXBrainApplication.java
@@ -0,0 +1,13 @@
+package com.eugeniojava.testxbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestXBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestXBrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eugeniojava/testxbrain/TestXBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugeniojava.testxbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestXBrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
